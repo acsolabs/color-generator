a5fcd1d2d6953dbce574a7b50146ed4a11c1f3e0
acsolabs <sofian.achbabi@gmail.com>
2026-03-12 13:54:14 +0100

    feat : change random background with function & display rgb color

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
 // Montrer sur le body la couleur rgb()
 
 //jouer un set interval 1sec pour que ça se joue tout seul ensuite
+//variables
+let red = 0;
+let green = 0;
+let blue = 0;
+const colorDisplay = document.getElementById("color-display");
+
+//functions
+const randomColor = (color) => {
+  color = Math.floor(Math.random() * 255);
+  return color;
+};
+
+backgroundColor = `rgb(${randomColor(red)}, ${randomColor(green)}, ${randomColor(blue)})`;
+
+document.body.style.background = backgroundColor;
+colorDisplay.innerText = backgroundColor;
