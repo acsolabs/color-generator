7c74abd93bb7edcd627519fa8437815157af77d6
acsolabs <sofian.achbabi@gmail.com>
2026-03-12 13:14:45 +0100

    init project

---
+++ index.js
@@ -0,0 +1,7 @@
+// Créer 3 variables pour stocker 3 chiffres aléatoires
+
+//Donner au body une couleur de fond en rgb()
+
+// Montrer sur le body la couleur rgb()
+
+//jouer un set interval 1sec pour que ça se joue tout seul ensuite
